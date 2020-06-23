e35c89b7e4a19eb1da08fbfc69bc416628e8bf30
okarpov <okarpov@saber3d.ru>
2020-06-23 12:20:09 +0300

    fix

---
+++ JSchemaGenerator/ArgumentsHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Fclp;
+
+namespace JSchemaGenerator
+{
+    public class Args
+    {
+        public List<string> Schemas { get; set; }
+        public string Namespace { get; set; }
+        public string OutputDir { get; set; }
+    }
+
+    internal static class ArgumentsHelper
+    {
+        public static FluentCommandLineParser<Args> CreateParser(this Args arguments)
+        {
+            var parser = new FluentCommandLineParser<Args>();
+
+            parser.Setup(a => a.Namespace)
+                .As('n', "ns")
+                .SetDefault("Schema")
+                .WithDescription("Generate csharp namespace name")
+                .Callback(@namespace => arguments.Namespace = @namespace);
+
+            parser.Setup(a => a.OutputDir)
+                .As('o', "output")
+                .WithDescription("Output directory name")
+                .Callback(output => arguments.OutputDir = output);
+
+            parser.Setup(a => a.Schemas)
+                .As('s', "schema")
+                .Required()
+                .WithDescription("Single/multiple schema file names")
+                .Callback(schemas => arguments.Schemas = schemas);
+
+            parser.SetupHelp("?", "h", "help")
+                .Callback(text => Console.WriteLine(text));
+
+            return parser;
+        }
+    }
+}
---
+++ JSchemaGenerator/SchemaGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace JSchemaGenerator
+{
+    internal static class SchemaGenerator
+    {
+        public static async Task GenerateCSharpFileFromSchema(string filename, Args args)
+        {
+            Console.WriteLine($@"Generate from: {filename}");
+            var input = Path.GetFullPath(filename);
+            Console.WriteLine($@"In: {input}");
+            var outFilename = $"{Path.GetFileNameWithoutExtension(filename)}.cs";
+            var output = Path.Combine((String.IsNullOrWhiteSpace(args.OutputDir) ? Path.GetDirectoryName(input) : Path.GetFullPath(args.OutputDir)) ?? String.Empty, outFilename);
+            Console.WriteLine($@"Out: {output}");
+            var jsonSchema = await JsonSchema.FromFileAsync(input);
+            var generator = new CSharpGenerator(jsonSchema, new CSharpGeneratorSettings()
+            {
+                Namespace = args.Namespace
+            });
+            var generateFile = generator.GenerateFile();
+            generateFile = generateFile.Replace(
+                @"[System.CodeDom.Compiler.GeneratedCode(""NJsonSchema"", ""10.1.21.0 (Newtonsoft.Json v9.0.0.0)"")]",
+                String.Empty);
+            await File.WriteAllTextAsync($"{output}", generateFile);
+        }
+    }
+}
